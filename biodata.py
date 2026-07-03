104aa75724a2d61342e223ed80213340e3ec2e0e
pangeranahmadkhalid69-spec <pangeranahmadkhalid69@gmail.com>
2026-07-03 22:13:41 +0700

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,14 @@
+print("=" * 30)
+print("BIODATA PRIBADI")
+print("=" * 30)
+
+nama = input("Nama : ")
+umur = input("Umur : ")
+hobi = input("Hobi : ")
+asal_kota = input("asal kota : ")
+
+print ("\n=== HASIL ===")
+print (f"Nama : {nama}")
+print (f"Umur : {umur}")
+print (f"Hobi : {hobi}")
+print (f"asal kota : {asal_kota}")
